4994aab7110dc1ee7ea12b27901fd5927c7eae69
peter <hanzp8023@163.com>
2022-02-27 00:39:19 +0800

    feat: call

---
+++ js/call/call.js
@@ -0,0 +1,28 @@
+// es3
+
+Function.prototype.call2 = function(context) {
+    context = context || window;
+    context.fn = this;
+
+    var args = [];
+    for (var i = 1; i < arguments.length; i++) {
+        args.push('arguments[' + i + ']');
+    }
+
+    var res = eval('context.fn(' + args + ')');
+
+    return res;
+}
+
+var obj = {
+    value: 3
+}
+
+function testCall(fir, sec) {
+    return this.value + fir + sec;
+}
+
+
+const res = testCall.call2(obj, 1, 5);
+
+console.log(res);
